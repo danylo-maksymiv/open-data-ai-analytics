6b0db0250bff26fa6e8f70e08195e6caf9238ddc
danylo-maksymiv <danylo.maksymiv.shi.2023@lpnu.ua>
2026-05-01 15:17:46 +0300

    Infra

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "Регіон AWS для розгортання"
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "Безкоштовний тип сервера"
+  default     = "t2.micro"
+}
+
+variable "app_port" {
+  description = "Порт, на якому працює веб-інтерфейс"
+  default     = 5050
+}
